2ab38068876b0228d2af23a83069a466bfa0d6df
Daniel Gould <dmgould310@gmail.com>
2020-07-07 12:59:12 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,6 @@
 def turn_count(board)
-  turns = 0
-  board.each do |space|
-    if space == "X" || space == "O"
-      turns += 1
-    end
-  end
-  return turns
-end
+turns = 0 
+
 
 def current_player(board)
   turns = turn_count(board)
